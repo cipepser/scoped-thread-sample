718c54936d900a80b05eb879a6bc8301a64a3944
cipepser <respepic@gmail.com>
2019-08-05 22:53:16 +0900

    remove target

--- standard/target/debug/deps/standard-7f245ac6d4fd4b83.d
+++
@@ -1,5 +0,0 @@
-/Users/cipepser/.go/src/github.com/cipepser/scoped-thread-sample/standard/target/debug/deps/standard-7f245ac6d4fd4b83: src/main.rs
-
-/Users/cipepser/.go/src/github.com/cipepser/scoped-thread-sample/standard/target/debug/deps/standard-7f245ac6d4fd4b83.d: src/main.rs
-
-src/main.rs:
--- standard/target/debug/standard.d
+++
@@ -1,1 +0,0 @@
-/Users/cipepser/.go/src/github.com/cipepser/scoped-thread-sample/standard/target/debug/standard.dSYM: /Users/cipepser/.go/src/github.com/cipepser/scoped-thread-sample/standard/src/main.rs
