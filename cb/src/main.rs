adad79a13da36ecd04db3928983ce0bb29725dc1
cipepser <respepic@gmail.com>
2019-08-19 23:03:43 +0900

    temporally commit

--- cb/src/main.rs
+++ cb/src/main.rs
@@ -1,7 +1,8 @@
 extern crate crossbeam;
 
-//use std::thread;
+use std::thread;
 use std::collections::VecDeque;
+use std::time::Duration;
 
 fn main() {
     let people = [
@@ -17,14 +18,18 @@ fn main() {
 
     crossbeam::scope(|scope| {
         for person in &people {
-            let mut queue = queue.clone();
+//            let mut queue = queue.clone();
             let h = scope.spawn(move |_| {
                 println!("{:?}", person);
-//                queue.push_back(person.to_string());
+                let s: 'static String =  "1".to_string();
+                queue.push_back(s);
+//                queue.push_back(person.clone());
             });
 //            h.join();
         }
+        scope.spawn(move |_| {
+            thread::sleep(Duration::from_secs(1));
+            println!("{:?}", queue);
+        });
     }).unwrap();
-
-    println!("{:?}", queue);
 }
